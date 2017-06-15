ae6a701e19c3e38171ab1ed0d0641163668c5fbd
Shayne Meyer <shayne.meyer@gmail.com>
2017-06-14 21:07:04 -0700

    finished form field macros

---
+++ manage.py
@@ -0,0 +1,15 @@
+import os, sys
+from flask_script import Manager, Server
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from flask_blog import app
+
+manager = Manager(app)
+
+manager.add_command('runserver', Server(
+    use_debugger=True,
+    use_reloader=True,
+    port=5000
+))
+
+if __name__ == "__main__":
+    manager.run()
---
+++ settings.py.example.py
@@ -0,0 +1,10 @@
+import os
+
+SECRET_KEY = 'something'
+DEBUG = True
+DB_USERNAME = ''
+DB_PASSWORD = ''
+BLOG_DATABASE_NAME = ''
+DB_HOST = ''
+DB_URI = "mysql+pymsql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, BLOG_DATABASE_NAME)
+SQLALCHEMY_DATABASE_URI = DB_URI
