89b8bb8402004b7e5c89a7ff67865d2f688070f7
Shayne Meyer <shayne.meyer@gmail.com>
2017-06-16 05:37:08 -0700

    fixed markdown error

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from markdown import Markdown
+from flaskext.markdown import Markdown
 
 app = Flask(__name__)
 app.config.from_object('settings')
