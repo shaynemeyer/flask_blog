bc1a3c2edc10f2f131d8d87b20c2085321100bc0
Shayne Meyer <shayne.meyer@gmail.com>
2017-06-16 06:40:51 -0700

    added code to save post to db

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,10 @@ from flask import render_template, redirect, flash, url_for, session, abort
 from blog.form import SetupForm, PostForm
 from flask_blog import db
 from author.models import Author
-from blog.models import Blog
+from blog.models import Blog, Category, Post
 from author.decorators import login_required, author_required
 import bcrypt
+from slugify import slugify
 
 
 @app.route('/')
@@ -61,6 +62,26 @@ def setup():
 @author_required
 def post():
     form = PostForm()
+    if form.validate_on_submit():
+        if form.new_category.data:
+            new_category = Category(form.new_category.data)
+            db.session.add(new_category)
+            db.session.flush()
+            category = new_category
+        elif form.category.data:
+            category_id = form.category.get_pk(form.category.data)
+            category = Category.query.filter_by(id=category_id).first()
+        else:
+            category = None
+        blog = Blog.query.first()
+        author = Author.query.filter_by(username=session['username']).first()
+        title = form.title.data
+        slug = slugify(title)
+        post = Post(blog, author, title, body, category, slug)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('admin'))
+
     return render_template('blog/post.html', form=form)
 
 
